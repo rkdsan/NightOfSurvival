6da36a86dad57771cf5e63f683bdfa515e48f994
ddoddodododdo <rkdtks6128@gmail.com>
2022-06-07 22:19:03 +0900

    Refactor: Prologue

--- Assets/Scripts/Prologue/CutScene.cs
+++ Assets/Scripts/Prologue/CutScene.cs
@@ -2,26 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CutScene : MonoBehaviour
 {
-    public List<GameObject> images;
+    public List<Image> images;
 
-    public bool isLast = false;
+    [HideInInspector] public bool isLast = false;
     private int imageIndex = 0;
+    private bool isChanging = false;
 
     private void OnEnable()
     {
         imageIndex = 0;
         foreach(var img in images)
         {
-            img.SetActive(false);
+            img.color = Color.clear;
         }
     }
 
     public void TurnOnNextCut()
     {
-        images[imageIndex++].SetActive(true);
+        if (!isChanging)
+        {
+            isChanging = true;
+            images[imageIndex++].DOColor(Color.white, 0.5f).OnComplete(() => isChanging = false);
+        }
+
 
         isLast = imageIndex == images.Count;
     }
