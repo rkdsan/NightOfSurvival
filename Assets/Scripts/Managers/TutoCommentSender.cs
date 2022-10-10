1eedda4d92db1634fcf9b711ba45bf2e89c20a18
ddoddodododdo <rkdtks6128@gmail.com>
2022-10-10 21:49:17 +0900

    여우누이 구역 안내문구 생성

--- Assets/Scripts/Managers/TutoCommentSender.cs
+++ Assets/Scripts/Managers/TutoCommentSender.cs
@@ -6,13 +6,20 @@ public class TutoCommentSender : MonoBehaviour
 {
     public static TutorialManager tutoManager;
     public string comment;
+    public bool IsResumable = false;
+
+    private void Reset()
+    {
+        IsResumable = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             tutoManager.ShowTutoWindow(comment);
-            Destroy(gameObject);
+            if(!IsResumable)
+                Destroy(gameObject);
         }
     }
 
